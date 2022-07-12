d221bdd99ba29450210c5dbb3304b28d31424e6c
OHJINSEONG <ojseong0828@naver.com>
2022-07-12 13:09:11 +0900

    두번쨰 코딩도장
    제발되라! 깃에대해 많이 공부하고 제출안되는 원리 파악완료

---
+++ BMI.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+        System.out.println("BMI 측정기");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("신장(m): ");
+        double answer1 = scanner.nextDouble();
+
+        System.out.print("체중(kg): ");
+
+        double answer2 = scanner.nextDouble();
+
+        double x = answer2 / Math.pow(answer1, 2);
+
+
+        if ((x < 18.5) || (x == 18.5)) {
+            System.out.println("비만도 결과: 저체중");
+            System.out.println("BMI: " + x);
+            return;
+
+
+        }
+        if ((x < 23) || (x == 23)) {
+            System.out.println("비만도 결과: 정상");
+            System.out.println("BMI: " + x);
+            return;
+        }
+        if ((x < 25) || (x == 25)) {
+            System.out.println("비만도 결과: 과체중");
+            System.out.println("BMI: " + x);
+            return;
+        }
+        if (x > 25) {
+            System.out.println("비만도 결과: 비만");
+            System.out.println("BMI: " + x);
+            return;
+        }
+
+
+    }
+}
