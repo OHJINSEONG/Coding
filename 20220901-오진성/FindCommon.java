b0040738b0ac0beb94e1012fb83155de132a8e77
OHJINSEONG <ojseong0828@naver.com>
2022-09-01 09:58:40 +0900

    20220901-findCommon-오진성

---
+++ 20220901-오진성/FindCommon.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = {};
+        int maximumDivider = 0;
+        long minmumTimer = 0;
+        int index = 1;
+        boolean find = true;
+        if (n <= m) {
+            for (int i = m; i > 0; i -= 1) {
+                if (n % i == 0 && m % i == 0) {
+                    maximumDivider = i;
+                    break;
+                }
+            }
+        }
+
+        if (n > m) {
+            for (int i = n; i > 0; i -= 1) {
+                if (n % i == 0 && m % i == 0) {
+                    maximumDivider = i;
+                    break;
+                }
+            }
+        }
+
+        while (find) {
+            if (m >= n) {
+                if ((index * m) % n == 0) {
+                    minmumTimer = index * m;
+                    find = false;
+                }
+            }
+            if (m < n) {
+                if ((index * n) % m == 0) {
+                    minmumTimer = index * n;
+                    find = false;
+                }
+            }
+            index += 1;
+        }
+
+        answer = new int[]{maximumDivider, (int) minmumTimer};
+        return answer;
+    }
+}
