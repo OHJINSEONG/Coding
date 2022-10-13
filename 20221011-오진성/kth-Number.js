abaab4f5e416bbfd37ac57f80f23074b1d93fe37
OHJINSEONG <ojseong0828@naver.com>
2022-10-13 10:47:09 +0900

    수정

---
+++ 20221011-오진성/kth-Number.js
@@ -0,0 +1,6 @@
+function solution(array, commands) {
+    const answer= commands.map((element)=> element[0]===element[1] ? array[element[0]-1]
+    : array.slice(element[0]-1,element[1]).sort((a,b) => a - b)[element[2]-1])
+     return answer;
+ }
+ 
